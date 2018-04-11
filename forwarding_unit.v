0b3678582ded2b39d30e6af813af7423c96c0e2b
thatseansannitoguy <sean.sannito@gmail.com>
2018-04-11 15:08:09 -0500

    SUPPER CUMMIT FASE 2 no @ciskokid7  ruined it
    Co-Authored-By: Nate Ciske <ciskokid7@users.noreply.github.com>

--- forwarding_unit.v
+++ forwarding_unit.v
@@ -1,4 +1,4 @@
-module FwardUnit(ID_EX_Rt, ID_EX_Rs, EX_MEM_Rd, MEM_WB_Rd, EX_MEM_RegWrite, MEM_WB_RegWrite, forward_src1, forwardb);
+module FwardUnit(ID_EX_Rt, ID_EX_Rs, EX_MEM_Rd, MEM_WB_Rd, EX_MEM_RegWrite, MEM_WB_RegWrite, forward_src1, forward_src2);
 
 //register addr for Rt, Rs, Rd in various pipline stages
 input [3:0] ID_EX_Rt, ID_EX_Rs, EX_MEM_Rd, MEM_WB_Rd;
@@ -9,23 +9,32 @@ input EX_MEM_RegWrite, MEM_WB_RegWrite;
 //condition codes for forwarded values for src 1 and 2 for ALU 
 output reg [1:0] forward_src1, forward_src2;
 
-localparam NO_HAZARD  = 2'b00;
+//hazard condition code params
 localparam EX_HAZARD  = 2'b10;
 localparam MEM_HAZARD = 2'b01;
+localparam NO_HAZARD  = 2'b00;
 
 always @(*) begin
-	if ((EX_MEM_RegWrite & |EX_MEM_Rd) & (EX_MEM_Rd == ID_EX_Rs)) begin
+	/*forward_src1 (Rs) logic */
+	//if EX/MEM RegWrite control is asserted and it Rd is the same as ID/EX Rs -> execution hazard
+	if ((EX_MEM_RegWrite) && (EX_MEM_Rd == ID_EX_Rs)) begin
 		forward_src1 <= EX_HAZARD;
-	end else if ((MEM_WB_RegWrite & |MEM_WB_Rd) & (MEM_WB_Rd == ID_EX_Rs)) begin
+	//if MEM/WB RegWrite is asserted and its Rd is the same as ID/EX Rs -> memory hazard
+	end else if ((MEM_WB_RegWrite) && (MEM_WB_Rd == ID_EX_Rs)) begin
 		forward_src1 <= MEM_HAZARD;
+	//condition code for no hazard in forward_src1
 	end else begin
 		forward_src1 <= NO_HAZARD;
 	end
 
-	if ((EX_MEM_RegWrite & |EX_MEM_Rd) & (EX_MEM_Rd == ID_EX_Rt)) begin
+	/*forward_src2 (Rd) logic */
+	//if EX/MEM RegWrite control is asserted and its Rd is the same as ID/EX Rt -> execution hazard
+	if ((EX_MEM_RegWrite) && (EX_MEM_Rd == ID_EX_Rt)) begin
 		forward_src2 <= EX_HAZARD;
-	end else if ((MEM_WB_RegWrite & |MEM_WB_Rd) & (MEM_WB_Rd == ID_EX_Rt)) begin
+	//if MEM/WB RegWrite is asserted and its Rd is the same as ID/EX Rt -> memory hazard
+	end else if ((MEM_WB_RegWrite) && (MEM_WB_Rd == ID_EX_Rt)) begin
 		forward_src2 <= MEM_HAZARD;
+	//condition code for no hazard in forward_src2
 	end else begin
 		forward_src2 <= NO_HAZARD;
 	end
--- hazard_detect.v
+++ hazard_detect.v
@@ -3,16 +3,15 @@ module HDU(IF_ID_Opcode, IF_ID_Rs, IF_ID_Rt, ID_EX_Rt, ID_EX_MemRead, pc_write, 
 input [3:0] IF_ID_Opcode, IF_ID_Rs, IF_ID_Rt, ID_EX_Rt;
 input ID_EX_MemRead;
 
-output pc_write, IF_ID_WriteInstr, stall;
+output reg pc_write, IF_ID_WriteInstr, stall;
 
-localparam ON 	= 1'b1;
+localparam ON  = 1'b1;
 localparam OFF = 1'b0;
-
 
 always @(*) begin
 	if (IF_ID_Opcode == 4'b1111) begin //IF current opcode is HLT, then stall
 		pc_write = OFF;
-		if_id_write = OFF;
+		IF_ID_WriteInstr = OFF;
 		stall = ON;
 	end else if (ID_EX_MemRead) begin //in the event ID/EX is reading from mem
 		//IF the target currently in ID/EX is the same as the source of IF/ID
@@ -20,16 +19,16 @@ always @(*) begin
 		//then stall and do not write
 		if (ID_EX_Rt == IF_ID_Rs || ID_EX_Rt == IF_ID_Rt) begin
 					pc_write = OFF;
-					if_id_write = OFF;
+					IF_ID_WriteInstr = OFF;
 					stall = ON;
 		end else begin //hazard condition not met
 					pc_write = ON;
-					if_id_write = ON;
+					IF_ID_WriteInstr = ON;
 					stall = OFF;
 		end
 	end else begin //no hazard found
 		pc_write = ON;
-		if_id_write = ON;
+		IF_ID_WriteInstr = ON;
 		stall = OFF;
 	end
 end
